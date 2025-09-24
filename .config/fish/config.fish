6c3fda43f99136976c7d9ba311edced30005d64e
askar <askar@e.email>
2025-09-24 22:31:33 +0300

    update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,14 +3,12 @@ if status is-interactive
     function fish_greeting
         fastfetch -c examples/8
     end
+    # set -g fish_autosuggestion_enabled 0
+    set -U fish_user_paths (go env GOPATH)/bin $fish_user_paths # add ~/go/bin to PATH
     set -gx EDITOR (type -p helix)
     export EDITOR=helix
 
     set fish_color_autosuggestion white
-
-    # TDLib path to write telegram client
-    # set CGO_CFLAGS -I/home/kitten/code/td/tdlib/include
-    # set CGO_LDFLAGS "-Wl,-rpath,/home/kitten/code/td/tdlib/lib -L/home/kitten/code/td/tdlib/lib -ltdjson"
 
     # to run electron-based apps directly on wayland
     set ELECTRON_OZONE_PLATFORM_HINT auto
